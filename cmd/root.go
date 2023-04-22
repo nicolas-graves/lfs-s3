a98414b79d9e4ce3edbb6e417442d8c61567bdc5
Nicolas Graves <ngraves@ngraves.fr>
2023-04-22 11:04:41 +0200

    Complete rewrite.

---
+++ cmd/root.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"gitlab.com/ngraves/git-lfs-s3/service"
+)
+
+var (
+	printVersion bool
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd *cobra.Command
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	RootCmd = &cobra.Command{
+		Use:   "git-lfs-s3",
+		Short: "git-lfs custom transfer adapter to store all data in a S3 container",
+		Long: `git-lfs-s3 treats a S3 bucket as the remote store for LFS object
+		data.`,
+		Run: rootCommand,
+	}
+
+	RootCmd.Flags().BoolVarP(&printVersion, "version", "", false, "Print version")
+	RootCmd.SetUsageFunc(usageCommand)
+
+}
+
+func usageCommand(cmd *cobra.Command) error {
+	usage := `
+Usage:
+  git-lfs-s3 [options]
+
+Options:
+  --version    Report the version number and exit
+
+Note:
+  This tool should only be called by git-lfs as documented in Custom Transfers:
+  https://github.com/git-lfs/git-lfs/blob/master/docs/custom-transfers.md
+
+  The arguments should be provided via gitconfig at lfs.customtransfer.<name>.args
+`
+	fmt.Fprintf(os.Stderr, usage)
+	return nil
+}
+
+func rootCommand(cmd *cobra.Command, args []string) {
+
+	if printVersion {
+		os.Stderr.WriteString(fmt.Sprintf("git-lfs-s3 %v\n", Version))
+		os.Exit(0)
+	}
+
+	service.Serve(os.Stdin, os.Stdout, os.Stderr)
+}
---
+++ service/service.go
@@ -0,0 +1,151 @@
+package service
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/sinbad/lfs-folderstore/api"
+	"github.com/sinbad/lfs-folderstore/util"
+)
+
+type Message struct {
+	Event  string  `json:"event"`
+	Oid    string  `json:"oid"`
+	Size   *int64  `json:"size,omitempty"`
+	Path   string  `json:"path,omitempty"`
+	Action string  `json:"action,omitempty"`
+	Error  *Error  `json:"error,omitempty"`
+}
+
+type Error struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func Serve(stdin io.Reader, stdout, stderr io.Writer) {
+	scanner := bufio.NewScanner(stdin)
+	writer := bufio.NewWriter(stdout)
+	errWriter := bufio.NewWriter(stderr)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		var req api.Request
+		if err := json.Unmarshal([]byte(line), &req); err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("Error reading input:", err))
+			return
+		}
+
+		switch req.Event {
+		case "init":
+			resp := &api.InitResponse{}
+			api.SendResponse(resp, writer, errWriter)
+		case "download":
+			util.WriteToStderr(fmt.Sprintf("Received download request for %s\n", req.Oid), errWriter)
+			retrieve(req.Oid, req.Size, req.Action, writer, errWriter)
+		case "upload":
+			util.WriteToStderr(fmt.Sprintf("Received upload request for %s\n", req.Oid), errWriter)
+			store(req.Oid, req.Size, req.Action, writer, errWriter)
+		case "terminate":
+			util.WriteToStderr("Terminating test custom adapter gracefully.\n", errWriter)
+			break
+		}
+	}
+}
+
+func createS3Client() *s3.Client {
+	region := os.Getenv("AWS_REGION")
+	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	endpointURL := os.Getenv("AWS_S3_ENDPOINT")
+
+	cfg, _ := config.LoadDefaultConfig(context.TODO(),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(
+			func(service, _ string) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: endpointURL, SigningRegion: region}, nil
+			})),
+		config.WithCredentialsProvider(aws.CredentialsProviderFunc(func(context.Context) (aws.Credentials, error) {
+			return aws.Credentials{
+				AccessKeyID:     accessKey,
+				SecretAccessKey: secretKey,
+			}, nil
+		})),
+	)
+
+	return s3.NewFromConfig(cfg)
+}
+
+func retrieve(oid string, size int64, action *api.Action, writer, errWriter *bufio.Writer) {
+	client := createS3Client()
+	bucketName := os.Getenv("S3_BUCKET")
+
+	localPath := fmt.Sprintf("%s.tmp", oid)
+	file, err := os.Create(localPath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	downloader := manager.NewDownloader(client, func(d *manager.Downloader) {
+		d.PartSize = 10 * 1024 * 1024     // 10 MB part size
+		d.Concurrency = 3                 // Concurrent downloads
+	})
+
+	_, err = downloader.Download(context.Background(), file, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(oid),
+	})
+
+	if err != nil {
+		return
+	}
+
+	complete := &api.TransferResponse{Event: "complete", Oid: oid, Path: localPath, Error: nil}
+	err = api.SendResponse(complete, writer, errWriter)
+	if err != nil {
+		util.WriteToStderr(fmt.Sprintf("Unable to send completion message: %v\n", err), errWriter)
+	}
+}
+
+func store(oid string, size int64, action *api.Action, writer, errWriter *bufio.Writer) {
+	client := createS3Client()
+	bucketName := os.Getenv("S3_BUCKET")
+
+	localPath := ".git/lfs/objects/" + oid[:2] + "/" + oid[2:4] + "/" + oid
+	file, err := os.Open(localPath)
+	if err != nil {
+		util.WriteToStderr(fmt.Sprintf("Error opening file: %v\n", err), errWriter)
+		return
+	}
+	defer file.Close()
+
+	uploader := manager.NewUploader(client, func(u *manager.Uploader) {
+		u.PartSize = 10 * 1024 * 1024     // 10 MB part size
+		u.LeavePartsOnError = true        // Keep uploaded parts on error
+		u.Concurrency = 3                 // Concurrent uploads
+	})
+
+	_, err = uploader.Upload(context.Background(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(oid),
+		Body:   file,
+	})
+
+	if err != nil {
+		util.WriteToStderr(fmt.Sprintf("Error uploading file: %v\n", err), errWriter)
+		return
+	}
+
+	complete := &api.TransferResponse{Event: "complete", Oid: oid, Error: nil}
+	err = api.SendResponse(complete, writer, errWriter)
+	if err != nil {
+		util.WriteToStderr(fmt.Sprintf("Unable to send completion message: %v\n", err), errWriter)
+	}
+}
