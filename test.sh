9d34106e2577f645683063a0ddf41f4b508ee29b
Nicolas Graves <ngraves@ngraves.fr>
2023-06-29 11:36:26 +0200

    More sensible binary production.

--- test.sh
+++ test.sh
@@ -10,9 +10,9 @@ fi
 ENVRC="$1"
 
 # Use a variant of the program to see input and output to the program.
-go build main.go &&\
-echo -e "#!/usr/bin/env sh\n\ntee -a ../input.log | ../../main --debug 2> ../error.log | tee -a ../output.log >&1\n" > main.sh &&\
-chmod +x main.sh &&\
+go build &&\
+echo -e "#!/usr/bin/env sh\n\ntee -a ../input.log | ../../lfs-s3 --debug 2> ../error.log | tee -a ../output.log >&1\n" > lfs-s3.sh &&\
+chmod +x lfs-s3.sh &&\
 rm -rf test &&\
 mkdir test &&\
 cd test && (
@@ -29,7 +29,7 @@ cd test && (
       git lfs track "*.bin"
       git add .gitattributes
       git commit -m "Adding .gitattributes"
-      git config --add lfs.customtransfer.lfs-s3.path ../../main.sh
+      git config --add lfs.customtransfer.lfs-s3.path ../../lfs-s3.sh
       git config --add lfs.standalonetransferagent lfs-s3
       git config --add lfs.concurrenttransfers 2
       dd if=/dev/urandom of=blob1.bin bs=1024 count=1024
@@ -43,7 +43,7 @@ cd test && (
   git clone --progress fake-remote-repo local-repo-dup &&\
     cd local-repo-dup && (
       git lfs install --local
-      git config --add lfs.customtransfer.lfs-s3.path ../../main.sh
+      git config --add lfs.customtransfer.lfs-s3.path ../../lfs-s3.sh
       git config --add lfs.standalonetransferagent lfs-s3
       git config --add lfs.concurrenttransfers 2
       source "$ENVRC"
