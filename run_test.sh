33daf6ddc85ee1d959b2ff89628a79bc2d2cb895
Daniel Kalmar <1355747+kalmard0@users.noreply.github.com>
2024-07-22 15:25:25 +0000

    Fix integration test for running as a github action.

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-go build -o test/izlfs-s3
+CGO_ENABLED=0 GOOS=linux go build -o test/izlfs-s3
 cd test
 docker build . -t izlfs-s3-test
-docker run --rm -it izlfs-s3-test
+docker run --rm -t izlfs-s3-test
